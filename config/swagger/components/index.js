c7689e8608f7b8dc31fa8cfaa0313a7c6958f17f
AjayB <bandhiya.ajay@gmail.com>
2025-09-10 00:33:34 +0530

    revert config changes

--- config/swagger/components/index.js
+++
@@ -1,7 +0,0 @@
-const securityComponents = require('./security.components');
-const parametersComponents = require('./parameters.components');
-
-module.exports = {
-  securitySchemes: securityComponents.securitySchemes,
-  parameters: parametersComponents.parameters
-};
